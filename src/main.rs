5692132dd92bc8ae3eed51d1ad3d946350494347
Shoaib Merchant <shoaibmerchant@Shoaibs-MacBook-Pro.local>
2022-10-31 19:07:54 +0530

    chore(fix): added bevy hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!")
+    App::new().add_system(hello_world_system).run();
 }
+
+fn hello_world_system() {
+    println!("hello world");
+}
+
