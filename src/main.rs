6c404b8b36c75597e11ebb3c31c8cb52d5f044a6
Shoaib Merchant <shoaibmerchant@Shoaibs-MacBook-Pro.local>
2022-11-02 11:10:22 +0530

    added window for bevy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_system(hello_world_system).run();
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(hello_world_system).run();
 }
 
 fn hello_world_system() {
